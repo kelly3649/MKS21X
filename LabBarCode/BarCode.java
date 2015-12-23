be4600d247f5f47f57c42718aca78d07557b3015
Kelly Wang <kwang6@stuy.edu>
2015-12-22 21:15:23 -0500

    finish half of Lab, got exceptions to work

--- LabBarCode/BarCode.java
+++ LabBarCode/BarCode.java
@@ -1,4 +1,7 @@
-public class BarCode implements Comparable{
+import java.lang.*;
+import java.util.*;
+public class BarCode{
+//implements Comparable{
 // instance variables
    private String _zip;
    private int _checkDigit;
@@ -8,27 +11,53 @@ public class BarCode implements Comparable{
 //postcondition: throws a runtime exception zip is not the correct length
 //               or zip contains a non digit
 //               _zip and _checkDigit are initialized.
-public BarCode(String zip) {}
-
+    public BarCode(String zip){
+	_zip = zip;
+	if(_zip.length() != 5){
+	    throw new RuntimeException();
+	}
+	for(int i = 0;i<_zip.length();i++){
+	    if(!Character.isDigit(_zip.charAt(i))){
+		throw new RuntimeException();
+	    }
+	}
+	int sum = 0;
+	for(int i = 0;i<_zip.length();i++){
+	    sum += _zip.charAt(i);
+	}
+	_checkDigit = sum & 10;
+	
+    }
 // postcondition: Creates a copy of a bar code.
-public BarCode(BarCode x){}
-
+public BarCode(BarCode x){
+    _zip = x._zip;
+    int sum = 0;
+    for(int i = 0;i<_zip.length();i++){
+	    sum += _zip.charAt(i);
+	}
+	_checkDigit = sum & 10;
+}
 
 //post: computes and returns the check sum for _zip
-private int checkSum(){}
+//private int checkSum(){}
 
 //postcondition: format zip + check digit + barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
-public String toString(){}
+public String toString(){
+    return _zip + _checkDigit;
+}
 
 
-public boolean equals(Object other){}
+//public boolean equals(Object other){}
 // postcondition: false if the object is not a BarCode, 
 // false if it is a non-matching barcode
 // true when they match.
 
 
-public int compareTo(Comparable other){}
+//public int compareTo(Comparable other){
 // postcondition: compares the zip + checkdigit 
+//    _zip - other._zip;
+//}
 
+    
 }
