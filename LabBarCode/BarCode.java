d75886747674d2fbde2b0afe81186eb2e13a60a8
Kelly Wang <kwang6@stuy.edu>
2015-12-22 21:52:10 -0500

    finish everything of lab, except compareTo. Kind of confused on how to do that

--- LabBarCode/BarCode.java
+++ LabBarCode/BarCode.java
@@ -39,16 +39,48 @@ public BarCode(BarCode x){
 }
 
 //post: computes and returns the check sum for _zip
-//private int checkSum(){}
-
+private int checkSum(){
+    	int sum = 0;
+	for(int i = 0;i<_zip.length();i++){
+	    sum += _zip.charAt(i);
+	}
+	_checkDigit = sum & 10;
+	return _checkDigit;
+}
 //postcondition: format zip + check digit + barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
 public String toString(){
-    return _zip + _checkDigit;
+    ArrayList<String> Ary = new ArrayList<String>();
+    Ary.add("||:::");
+    Ary.add(":::||");
+    Ary.add("::|:|");
+    Ary.add("::||:");
+    Ary.add(":|::|");
+    Ary.add(":|:|:");
+    Ary.add(":||::");
+    Ary.add("|:::|");
+    Ary.add("|::|:");
+    Ary.add("|:|::");
+    //    for(int i=0;i<Ary.size();i++){
+    //	System.out.print(Ary.get(i) + ", ");    
+    //}
+    String retString = "|";
+    for(int i=0;i<_zip.length();i++){
+	String digit = "" +  _zip.charAt(i);
+	retString += Ary.get(Integer.parseInt(digit));
+    }
+    retString += Ary.get(_checkDigit);
+    retString += "|";
+    return _zip + _checkDigit + " " + retString;
 }
 
 
-//public boolean equals(Object other){}
+public boolean equals(Object other){
+    if(other instanceof BarCode){
+	return toString() == other.toString();
+    }
+    return false;
+}
 // postcondition: false if the object is not a BarCode, 
 // false if it is a non-matching barcode
 // true when they match.
--- LabBarCode/Driver.java
+++ LabBarCode/Driver.java
@@ -1,15 +1,21 @@
 public class Driver{
     public static void main(String[]args){
-	BarCode b = new BarCode("01010");
+	BarCode b = new BarCode("01004");
 	System.out.println(b);
 	try{BarCode b2 = new BarCode("0301");
 	    //System.out.println(b2);
 	}catch(RuntimeException e){
 	    System.out.println("ERROR:num of digits wrong");
 	}
+	BarCode b2 = new BarCode("03010");
+	System.out.println(b2);
 	try{BarCode b3 = new BarCode("abacd");
 	}catch(RuntimeException e){
 	    System.out.println("ERROR:BarCode contains non-digits");
 	}
+	BarCode b3 = new BarCode("03010");
+	System.out.println(b3);
+	System.out.println(b.equals(b2));
+	System.out.println(b2.equals(b3));
     }
 }
