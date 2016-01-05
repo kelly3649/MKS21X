690a9257e9d9a71c5062c9d0638fbee9eff0bd31
Kelly Wang <kwang6@stuy.edu>
2016-01-04 21:11:44 -0500

    IT WORKS!

--- hwSorts/Driver.java
+++ hwSorts/Driver.java
@@ -3,13 +3,14 @@ public class Driver{
 	int[] ary = {-1,3,-2,-2,-2};
 	int[] arry = {8, 6, 7, 5, 3, 0, 9};
 	int[] array = {2,124,-5,13,0,0};
-	Sorts.printArray(arry);
 	Sorts.insertionSort(arry);
 	Sorts.printArray(arry);
-	Sorts.printArray(ary);
+	Sorts.selectionSort(arry);
+	Sorts.printArray(arry);
 	Sorts.insertionSort(ary);
 	Sorts.printArray(ary);
-	Sorts.printArray(array);
+	Sorts.selectionSort(ary);
+	Sorts.printArray(ary);
 	Sorts.insertionSort(array);
 	Sorts.printArray(array);
 	Sorts.selectionSort(array); //supposed to be the same result as above
